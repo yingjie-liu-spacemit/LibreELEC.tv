88550162a77a7c11b7822e3ca1c64ecde4a09821
liuyingjie <yingjie.liu@spacemit.com>
2026-08-20 11:30:05 +0800

    project/Spacemit: add glibc, nfs-utils, openssl overrides and nss patch
    - glibc: --enable-kernel=6.6.0 (match K1 kernel version)
    - nfs-utils: static link fix (ncursesw after -lreadline in LIBS)
    - openssl: add riscv64 linux64-riscv64 build target
    - nss: disable -Werror for gcc16 compatibility

---
+++ projects/Spacemit/packages/nfs-utils/package.mk
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
+#
+# SpacemiT K1 port: readline links statically and needs ncursesw (terminfo:
+# tputs/tgoto/tgetent/etc.) to be on the link line AFTER -lreadline.
+# autotools' LIBS is appended to the end of the link command, which gives
+# the correct order for static linking.
+
+PKG_NAME="nfs-utils"
+PKG_VERSION="2.9.2"
+PKG_SHA256="e1dd8a9c95af15492065942cc3b52b1339ffd586baa2280ed86c9d3dc4097e8c"
+PKG_LICENSE="GPL-2.0-or-later"
+PKG_SITE="http://www.linux-nfs.org/"
+PKG_URL="https://www.kernel.org/pub/linux/utils/nfs-utils/${PKG_VERSION}/nfs-utils-${PKG_VERSION}.tar.xz"
+PKG_DEPENDS_TARGET="toolchain keyutils libevent libnl libtirpc libxml2 readline rpcbind sqlite util-linux"
+PKG_LONGDESC="Linux NFS userland utility package"
+
+PKG_CONFIGURE_OPTS_TARGET="--disable-gss \
+                           --disable-nfsv41 \
+                           --disable-nfsdcld \
+                           --disable-nfsrahead \
+                           --disable-nfsdcltrack \
+                           --disable-ldap"
+
+pre_configure_target() {
+  cd ${PKG_BUILD}
+  rm -rf .${TARGET_NAME}
+  # readline is static; ncursesw must appear AFTER -lreadline on the link
+  # line. LIBS is appended at the very end by automake, giving the right
+  # order for static symbol resolution.
+  export LIBS="${LIBS} -lncursesw"
+}
+
+post_configure_target() {
+  libtool_remove_rpath libtool
+}
+
+makeinstall_target() {
+  mkdir -p "${INSTALL}/usr/sbin/"
+    cp -PR utils/mount/mount.nfs "${INSTALL}/usr/sbin/"
+    ln -s mount.nfs "${INSTALL}/usr/sbin/mount.nfs4"
+    ln -s mount.nfs "${INSTALL}/usr/sbin/umount.nfs"
+    ln -s mount.nfs "${INSTALL}/usr/sbin/umount.nfs4"
+}
