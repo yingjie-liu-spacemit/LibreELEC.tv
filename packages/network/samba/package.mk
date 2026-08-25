45f89a743d2c61cabeefae7bb5593b8e48f78e25
liuyingjie <yingjie.liu@spacemit.com>
2026-08-25 16:33:57 +0800

    samba: install private shared libs to sysroot
    copy_directory_of_links was not copying bin/shared/private to the
    sysroot, causing undefined reference errors (lpcfg_string_set@SAMBA,
    cli_smb2_fnum_is_posix etc.) when kodi links against libsmbclient/
    libsmbconf. The INSTALL side already copies private libs; mirror that
    for SYSROOT_PREFIX.

--- packages/network/samba/package.mk
+++ packages/network/samba/package.mk
@@ -140,6 +140,7 @@ copy_directory_of_links() {
 perform_manual_install() {
   mkdir -p ${SYSROOT_PREFIX}/usr/lib
     copy_directory_of_links ${PKG_BUILD}/bin/shared ${SYSROOT_PREFIX}/usr/lib
+    copy_directory_of_links ${PKG_BUILD}/bin/shared/private ${SYSROOT_PREFIX}/usr/lib
 
   mkdir -p ${INSTALL}/usr/lib
     copy_directory_of_links ${PKG_BUILD}/bin/shared ${INSTALL}/usr/lib
